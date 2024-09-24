c0ac60db5b5430c06e1e2f01ed1acab25ccc6ae5
akaBacsi <149259090+akaBacsi@users.noreply.github.com>
2024-09-24 20:56:22 -0300

    Update urls.py

--- appdocente/urls.py
+++ appdocente/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('crear_reserva/', views.crear_reserva, name='crear_reserva'),
     path('api/horario/<int:sala_id>/', views.obtener_horario_por_sala, name='obtener_horario_por_sala'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
